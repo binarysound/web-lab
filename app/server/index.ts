801712c4d3c14a084cc7da82ccc6b8a622083b0c
Jeongho Jeong <fiil12@hotmail.com>
2018-09-09 02:52:22 +0900

    [#6] Add basic socket.io example server logic

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,12 +1,24 @@
 import express from 'express'
+import http from 'http'
+import SocketIO from 'socket.io'
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static('build/client'))
+
+const server = http.createServer(app)
+
+const io = SocketIO(server)
+io.on('connection', (socket) => {
+  socket.emit('server event', { message: 'Hello, world!' })
+  socket.on('client event', (data) => {
+    /* tslint:disable-next-line:no-console */
+    console.log(data)
+  })
 })
 
-app.listen(PORT, function () {
+server.listen(PORT, () => {
+  /* tslint:disable-next-line:no-console */
   console.log(`App listening on port ${PORT}.`)
 })
